9066773cf424ffadb4f9b45702b3ffbe2ea72892
грайр <rpaup@internet.ru>
2025-10-10 19:53:17 +0200

    feat: Add logic to save form data to data.txt

--- www/process.php
+++ www/process.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
-$username = htmlspecialchars($_POST['playerName'] ?? ''); 
+$username = htmlspecialchars($_POST['playerName'] ?? ''); // Используем правильный ключ
 $email = htmlspecialchars($_POST['email'] ?? '');
 $_SESSION['username'] = $username;
 $_SESSION['email'] = $email;
+$line = $username . ";" . $email . "\n";
+file_put_contents("data.txt", $line, FILE_APPEND | LOCK_EX); // LOCK_EX - лучшая практика
 header("Location: index.php");
 exit();
